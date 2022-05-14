8a427e3247a2a38e4fee8880725667289b2673e1
mr.seif <09393009535m@>
2022-05-14 13:07:17 +0430

    add DateFormat utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,21 @@
 export const convertFromByteArrayToSrring = (item: ArrayBuffer) => {
   return 'data:image/jpeg;base64,' + btoa(new Uint8Array(item).reduce((data, byte) => data + String.fromCharCode(byte), ''));
 }
+
+export const dateFormat = (date: Date) => {
+  const dateObject = new Date(date);
+  const year = dateObject.getFullYear();
+  let month: string | number = dateObject.getMonth();
+  let day: string | number = dateObject.getDay();
+  if(day < 10) {
+    day = `0${day + 1}`
+  } else {
+    day++;
+  }
+  if(month < 10) {
+    month = `0${month + 1}`
+  } else {
+    month++;
+  }
+  return `${year}-${month}-${day}`
+}
